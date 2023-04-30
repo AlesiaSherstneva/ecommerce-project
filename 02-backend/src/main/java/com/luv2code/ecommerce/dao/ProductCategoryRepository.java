a17433a97f53b101cb5506a65fbcb054428c64da
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-04-30 23:01:31 +0300

    Integrate frontend and backend

--- 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.luv2code.ecommerce.dao;
 import com.luv2code.ecommerce.entities.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "product-category", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,8 @@ package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
