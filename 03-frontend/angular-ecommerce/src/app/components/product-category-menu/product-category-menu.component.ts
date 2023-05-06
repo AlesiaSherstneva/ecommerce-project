ca7c1e3e20ce918b90f2605d91d7b523cbe2c480
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-05-06 16:30:48 +0300

    Generate/define search component

--- 03-frontend/angular-ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductCategory } from 'src/app/common/product-category';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -8,7 +8,7 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-category-menu.component.css']
 })
 
-export class ProductCategoryMenuComponent {
+export class ProductCategoryMenuComponent implements OnInit {
   productCategories: ProductCategory[] = [];
 
   constructor(private productService: ProductService) { }
