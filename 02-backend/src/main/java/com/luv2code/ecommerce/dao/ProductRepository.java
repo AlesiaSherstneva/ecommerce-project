ccf5768e0d72add1327b345e4ab10eb2b49b847b
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-05-06 16:13:41 +0300

    Create searching method for products

--- 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
+@SuppressWarnings("unused")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
