759d6ed22e95464e48a1d8d83987300d5e64f369
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-01 17:34:07 +0300

    Add the functionality to display cart totals

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CartService } from 'src/app/services/cart.service';
 import { Luv2shopformService } from 'src/app/services/luv2shopform.service';
 import { Luv2shopValidators } from 'src/app/validators/luv2shop-validators';
 
@@ -12,6 +13,7 @@ import { Luv2shopValidators } from 'src/app/validators/luv2shop-validators';
 })
 export class CheckoutComponent implements OnInit {
   checkoutFormGroup!: FormGroup;
+
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
@@ -23,9 +25,11 @@ export class CheckoutComponent implements OnInit {
   billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
-    private luv2shopFormService: Luv2shopformService) { };
+    private luv2shopFormService: Luv2shopformService,
+    private cartService: CartService) { };
 
   ngOnInit(): void {
+    this.reviewCartDetails();
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('', [Validators.required, Validators.minLength(2), Luv2shopValidators.notOnlyWhitespace]),
@@ -80,6 +84,16 @@ export class CheckoutComponent implements OnInit {
         this.countries = data;
       }
     )
+  }
+
+  reviewCartDetails() {
+    this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    );
+
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    );
   }
 
   get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
