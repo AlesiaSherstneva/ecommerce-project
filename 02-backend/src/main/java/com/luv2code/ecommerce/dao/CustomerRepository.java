b63c84d1ade58847a6af42c9536ce4386a1fdcb5
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-03 16:59:58 +0300

    Create customer repository

---
+++ 02-backend/src/main/java/com/luv2code/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
