62a915b02fd0568d94d64e216bd08048ead05f1f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-04 23:08:13 +0300

    Create checkout controller

---
+++ 02-backend/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,24 @@
+package com.luv2code.ecommerce.controller;
+
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private final CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        return checkoutService.placeOrder(purchase);
+    }
+}
