18f0eb2e130a5ccaf2b654a56ab4c7f09dba26f0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-08 07:54:48 +0300

    Develop API configuration

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa9vo9ncieaf2skB5d7',
+        issuer: 'https://dev-08415111.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
