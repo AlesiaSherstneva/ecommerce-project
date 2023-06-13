b910a3c72136fdf24b045d4c54b9ecc7f036328e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-13 16:30:39 +0300

    Change session storage to local storage

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -9,7 +9,7 @@ export class CartService {
   cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
-  storage: Storage = sessionStorage;
+  storage: Storage = localStorage;
 
   constructor() {
     let data = JSON.parse(this.storage.getItem('cartItems'));
