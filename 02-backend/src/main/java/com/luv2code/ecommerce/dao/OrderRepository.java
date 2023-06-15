89d2decc7a276b63c157897dea38df8c2e06a9e4
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-15 18:28:47 +0300

    Create JPA repository for orders

---
+++ 02-backend/src/main/java/com/luv2code/ecommerce/dao/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entities.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+@SuppressWarnings("unused")
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
+}
