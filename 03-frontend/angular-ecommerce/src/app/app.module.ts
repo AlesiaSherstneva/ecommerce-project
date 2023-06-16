c15e8fcd509bb7b16a3df12ab6cc502bf79fa9de
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-16 15:16:47 +0300

    Generate/define order history component

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG, OktaAuthGuard } fro
 import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 const oktaConfig = myAppConfig.oidc;
 const oktaAuth = new OktaAuth(oktaConfig);
@@ -61,7 +62,8 @@ const routes: Routes = [
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
-    MembersPageComponent
+    MembersPageComponent,
+    OrderHistoryComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
