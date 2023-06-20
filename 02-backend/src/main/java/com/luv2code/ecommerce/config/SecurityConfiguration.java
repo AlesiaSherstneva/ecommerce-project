d1e3a387dc8e3610710de6258bc2004295652d6d
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-20 16:03:26 +0300

    Add user-friendly answer for 401 response

--- 02-backend/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.luv2code.ecommerce.config;
 
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +20,10 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer()
                 .jwt();
         http.cors();
+
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+        Okta.configureResourceServer401ResponseBody(http);
+
         return http.build();
     }
 }
