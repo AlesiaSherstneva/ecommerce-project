3ddd8daaf290eb177d131dde75624310e80ca2bd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-20 16:37:51 +0300

    Generate/define auth interceptor

---
+++ 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,33 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { request } from 'http';
+import { Observable, from, lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      const accessToken = this.oktaAuth.getAccessToken();
+
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+
+    return await lastValueFrom(next.handle(request));
+  }
+}
