7d0fd0dfa0f1b2be6b89c115ac269ac77bb65032
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-21 15:08:11 +0300

    Generate key and self-signed certificate on the frontend, update URI's

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa9vo9ncieaf2skB5d7',
         issuer: 'https://dev-08415111.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
