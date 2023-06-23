9cf075ad60e73961a7ac9606ba95d144a9cbdeb0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-23 12:24:56 +0300

    Replace hardcoded URL with environment URL

--- 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import { Observable, from, lastValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,8 @@ export class AuthInterceptorService implements HttpInterceptor {
   }
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-    const securedEndpoints = ['http://localhost:8080/api/orders'];
+    const theEndpoint = environment.luv2shopApiUrl + '/orders';
+    const securedEndpoints = [theEndpoint];
 
     if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
       const accessToken = this.oktaAuth.getAccessToken();
