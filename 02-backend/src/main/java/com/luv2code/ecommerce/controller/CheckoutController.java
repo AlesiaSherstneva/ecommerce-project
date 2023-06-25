792c7cbd14a15cccceb3ce500f50b17f55cb18bd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-25 17:51:27 +0300

    Create method for payment handling in checkout controller

--- 02-backend/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ 02-backend/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.luv2code.ecommerce.controller;
 
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +24,12 @@ public class CheckoutController {
     @PostMapping("/purchase")
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
         return checkoutService.placeOrder(purchase);
+    }
+
+    @PostMapping("payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentString = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentString, HttpStatus.OK);
     }
 }
