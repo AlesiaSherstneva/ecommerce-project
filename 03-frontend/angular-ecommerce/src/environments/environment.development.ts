4400b0eb236e7f030495c81ea978a2536ee92085
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-06-26 16:27:21 +0300

    Add stripe publishable key

--- 03-frontend/angular-ecommerce/src/environments/environment.development.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    luv2shopApiUrl: "https://localhost:8443/api"
+    luv2shopApiUrl: "https://localhost:8443/api",
+    stripePublishableKey: "pk_test_51NMWdrBXbUeHtf8b3fxdFlInK7YWSD2ToSRrPFY4qN6EerKu21gym6XCk7rkek4EjTXbe6HPJk6A2GGKJxYMgxu100ZrYzWi1Y"
 };
